7eea5c6d57d74997fe36c61baf3e16ea98890753
priyankarathnam <pcr43@cornell.edu>
2017-12-01 16:50:51 -0500

    added constants

--- alphabeta.py
+++ alphabeta.py
@@ -1,24 +1,25 @@
 import sys, math, numpy as np
 
 #number pieces in a row to win
-target = 3
-rows = 4
-cols = 4
+TARGET = 3
+ROWS = 4
+COLS = 4
+DEEPEST = 3
 next_actions = {}
 
 #initialize depth to 0
 def a_b_search(board, depth):
-	assert len(board) == rows
-	assert len(board[0]) == cols
+	assert len(board) == ROWS
+	assert len(board[0]) == COLS
 	v = max_value(board, -math.inf, math.inf, depth)
 	return result(board, next_actions[v])
 
 def max_value(board, a, b, depth):
-	if depth == 3 or is_win(board):
+	if depth == DEEPEST or is_win(board) or is_loss(board):
 		return utility(board)
 	v = -math.inf
 	for action in actions(board):
-		action_value = min_value(result(board, action), a, b, depth+1)
+		action_value = min_value(result(board, action, "X"), a, b, depth+1)
 		if depth == 0:
 			next_actions[action_value] = action
 		v = max(v, action_value)
@@ -28,11 +29,11 @@ def max_value(board, a, b, depth):
 	return v
 
 def min_value(board, a, b, depth):
-	if depth == 3 or is_loss(board):
+	if depth == DEEPEST or is_loss(board) or is_win(board):
 		return utility(board)
 	v = math.inf
 	for action in actions(board):
-		v = min(v, max_value(result(board, action), a, b, depth+1))
+		v = min(v, max_value(result(board, action, "O"), a, b, depth+1))
 		if v <= a:
 			return v
 		b = min(b,v)
@@ -41,7 +42,7 @@ def min_value(board, a, b, depth):
 #returns true if there is a four in a row (X's) on the board
 def is_win(board):
 	x_arr = []
-	for i in range(0, target):
+	for i in range(0, TARGET):
 		x_arr.append("X")
 	if x_arr in board:
 		return True
@@ -59,7 +60,7 @@ def is_win(board):
 #returns true if there is a four in a row (O's) on the board
 def is_loss(board):
 	o_arr = []
-	for i in range(0, target):
+	for i in range(0, TARGET):
 		o_arr.append("O")
 	if o_arr in board:
 		return True
@@ -78,15 +79,17 @@ def is_loss(board):
 def utility(board):
 	return
 
-#returns a list of numbers that represent possible actions (0-6)
+#returns a list of numbers that represent possible actions (number 0-6)
 def actions(board):
 	ret = []
-	for i in range(0, cols):
+	for i in range(0, COLS):
 		if board[0][i] == "":
 			ret.append(i)
 	return ret
 
 #returns resulting board after taking an action (number from 0-6)
-def result(board, action):
+#type = string "X" or string "O"
+def result(board, action, type):
 	return
+
 is_win([["","","","",],["","","",""],["","","",""],["X", "X", "X", "O"]])
